e69cddfbcfa394321c9da2f82d78518eadf70248
Rodrigo Gontijo <rodrigo.gontijo@hotmail.com>
2025-05-02 18:48:57 -0300

    Add application

---
+++ PlateRecognizer/app/src/main/java/com/example/reconhecedordeplacas/di/KoinModules.kt
@@ -0,0 +1,36 @@
+package com.example.reconhecedordeplacas.di
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val appModule = module {
+
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://api.platerecognizer.com/")
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor {
+                        val request = it.request().newBuilder()
+                            .addHeader("Authorization", "Token SUA_CHAVE_AQUI")
+                            .build()
+                        it.proceed(request)
+                    }
+                    .addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+                    })
+                    .build()
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PlateRecognizerApi::class.java)
+    }
+
+    single { PlateRecognizerClient(get()) }
+
+    viewModel { CameraViewModel(get()) }
+}
